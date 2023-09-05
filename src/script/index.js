3c82fbab9cb2a7932ea5d541b8f9e0746a5d5f49
Youness-Ailal <itsyounessaylal@gmail.com>
2023-09-05 18:03:30 +0100

    Init commit

---
+++ src/script/index.js
@@ -0,0 +1,103 @@
+// import iconLocation from "../assets/images/icon-location.svg";
+// console.log(iconLocation);
+// App Variables
+const ipText = document.querySelector("#ip");
+const cityText = document.querySelector("#city");
+const regionText = document.querySelector("#region");
+const countryText = document.querySelector("#country");
+const flagImg = document.querySelector(".result__flag-image");
+const ispText = document.querySelector(".isp");
+const utcText = document.querySelector(".utc");
+const spinner = document.querySelector(".spinner");
+
+const formBtn = document.querySelector(".header__btn");
+const form = document.querySelector(".header__form");
+
+// Map variables
+const map = L.map("map").setView([51, -0.09], 18);
+const customIcon = L.icon({
+  iconUrl: "assets/images/icon-location.svg",
+  iconSize: [50, 60],
+  iconAnchor: [16, 37],
+});
+// Get Local IP Address
+spinner.classList.remove("hidden");
+const resIp = await fetch(`https://api.ipify.org?format=json`);
+const { ip } = await resIp.json();
+if (ip) {
+  renderLocation("ipAddress", ip);
+} else {
+  spinner.classList.add("hidden");
+}
+// Get IP, Location, Timezone, ISP
+async function renderLocation(type, data) {
+  spinner.classList.remove("hidden");
+  try {
+    const dataRes = await (
+      await fetch(
+        `https://geo.ipify.org/api/v2/country,city?apiKey=at_pMCkCpy23Goiw8MsZZYF588QVkRuK&${type}=${data}`
+      )
+    ).json();
+
+    const { lat, lng, city, region, country, timezone } = dataRes.location;
+    const countryData = await fetch(
+      `https://restcountries.com/v3.1/alpha/${country}`
+    ).then(res => res.json());
+
+    const countryName = countryData[0].name.common || "Unkown";
+    const countryFlag = countryData[0].flags.png || "Unkown";
+    const { isp } = dataRes;
+
+    ipText.textContent = ip;
+    cityText.textContent = city;
+    regionText.textContent = region + " ";
+    countryText.textContent = countryName;
+    utcText.textContent = timezone;
+    flagImg.src = countryFlag;
+
+    ispText.textContent = isp;
+    displayMap(lat, lng);
+  } catch (err) {
+    alert("Invalid IP address or domain!");
+    console.log(err);
+  } finally {
+    spinner.classList.add("hidden");
+  }
+}
+
+//Handles form input submission
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const input = document.querySelector(".header__input").value;
+  if (/\d+\.\d+/.test(input)) {
+    renderLocation("ipAddress", input);
+  } else {
+    renderLocation("domain", input);
+  }
+});
+
+// Display Map
+function displayMap(lat, lon) {
+  map.setView([lat, lon], 18);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.marker([lat, lon], { icon: customIcon }).addTo(map);
+}
+
+//Give country info
+async function countryInfo(code) {
+  try {
+    const data = await fetch(
+      `https://restcountries.com/v3.1/alpha/${code}`
+    ).then(res => res.json());
+    return {
+      name: data[0].name.common,
+      flag: data[0].flags.png,
+    };
+  } catch (err) {
+    alert("invalid Ip Address or domain");
+  }
+}
